645786cd1a7f92a79dd756e6943286b41ae4ddf9
varun sachdeva <sachdevavarun7@gmail.com>
2021-08-24 22:45:36 +0530

    auction ui

--- src/components/LiveAuctions.jsx
+++ src/components/LiveAuctions.jsx
@@ -11,67 +11,51 @@ import { Typography } from "@material-ui/core";
 import { TextField } from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
-    root: {
-        width: 500,
-        padding: 100,
-        margin: "0 30%",
-    },
-    media: {
-        height: "200px", // 16:9
-    },
+	root: {
+		width: 500,
+		padding: 100,
+		margin: "0 30%",
+	},
+	media: {
+		height: "200px", // 16:9
+	},
 }));
 
 function LiveAuctions({ gameState, getNextAuctionObj }) {
-    const classes = useStyles();
-    const [artifact, setArtifact] = useState();
-    const [auctionObj, setAuctionObj] = useState();
+	const classes = useStyles();
+	const [artifact, setArtifact] = useState();
+	const [auctionObj, setAuctionObj] = useState();
 
-    useEffect(() => {
-      socket.on("startNextAuction", auctionObj => {
-        setAuctionObj(auctionObj);
-      });
-    }, [auctionObj]);
+	useEffect(() => {
+		socket.on("startNextAuction", auctionObj => {
+			setAuctionObj(auctionObj);
+		});
+	}, [auctionObj]);
 
-    const renderArtifacts = () => {
-        let { auctions } = gameState;
-        setArtifact(auctions.artifacts[0]);
-        return (
-            <Card key={artifact.id}>
-              <CardHeader title={artifact.name} />
-              <CardMedia className={classes.media} component="img" image={`${artifact.imageURL}`} title={artifact.name} />
-              <CardContent>
-                  <Typography color="textSecondary">Original price:{artifact.originalValue}</Typography>
-                  <Typography>Winning Bid: {artifact.bid.buyingPrice}</Typography>
-                  <Typography>Winning Team: {artifact.bid.buyingTeam}</Typography>
-              </CardContent>
-              <CardActions disableSpacing>
-                  <TextField name="bidAmount" placeholder="Bidding Amount" variant="outlined" />
-                  <Button variant="contained" color="secondary">
-                      Bid
-                  </Button>
-              </CardActions>
-            </Card>
-        );
-    };
-    return (
-      <div>
-        {
-          auctionObj &&
-          <>
-            <p>
-              {auctionObj.artist}
-            </p>
-            <p>
-              {auctionObj.name}
-            </p>
-            <p>
-              {auctionObj.imageURL}
-            </p>
-          </>
-        }
-        <Button onClick={getNextAuctionObj}>Next</Button>
-      </div>
-    )
+	return (
+		<div className={classes.root}>
+			{auctionObj && (
+				<>
+					<Card key={auctionObj.id}>
+						<CardHeader title={auctionObj.name} />
+						<CardMedia className={classes.media} component="img" image={`${auctionObj.imageURL}`} title={auctionObj.name} />
+						<CardContent>
+							<Typography color="textSecondary">Original price:{auctionObj.originalValue}</Typography>
+							<Typography>Winning Bid: {auctionObj.bid.buyingPrice}</Typography>
+							<Typography>Winning Team: {auctionObj.bid.buyingTeam}</Typography>
+						</CardContent>
+						<CardActions disableSpacing>
+							<TextField name="bidAmount" placeholder="Bidding Amount" variant="outlined" />
+							<Button variant="contained" color="secondary">
+								Bid
+							</Button>
+						</CardActions>
+					</Card>
+				</>
+			)}
+			<Button onClick={getNextAuctionObj}>Next</Button>
+		</div>
+	);
 }
 
 export default LiveAuctions;
