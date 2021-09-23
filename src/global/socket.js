474eb126eef75597f2e09c093bf4e0bb6c0bfecb
khyatith <thakur.khyati70@gmail.com>
2021-09-23 16:03:29 -0700

    Add firebase

--- src/global/socket.js
+++ src/global/socket.js
@@ -1,10 +1,10 @@
 import { io } from 'socket.io-client';
 
 // eslint-disable-next-line import/prefer-default-export
-export const socket = io.connect('http://localhost:3001', { transports: ['websocket', 'polling', 'flashsocket'] });
+export const socket = io.connect('https://art-quest-server.herokuapp.com/', { transports: ['websocket', 'polling', 'flashsocket'] });
 
 export const leaderboardSocket = io.connect(
-  'http://localhost:3001/leaderboard-namespace',
+  'https://art-quest-server.herokuapp.com/leaderboard-namespace',
   {
     transports: ['websocket', 'polling', 'flashsocket'],
   },
