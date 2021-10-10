ae37fa5e6d0c8d4bf5cc764148e8d5388110fc05
khyatith <thakur.khyati70@gmail.com>
2021-10-10 01:34:36 -0700

    redo client

--- src/global/socket.js
+++ src/global/socket.js
@@ -1,11 +1,12 @@
+/* eslint-disable import/prefer-default-export */
 import { io } from 'socket.io-client';
 
 // eslint-disable-next-line import/prefer-default-export
-export const socket = io.connect('https://art-quest-server.herokuapp.com/', { transports: ['websocket', 'polling', 'flashsocket'] });
 
-export const leaderboardSocket = io.connect(
-  'https://art-quest-server.herokuapp.com/leaderboard-namespace',
-  {
-    transports: ['websocket', 'polling', 'flashsocket'],
-  },
-); // the "leaderboards" namespace
+export const socket = io.connect('https://art-quest-server.herokuapp.com/', {
+  withCredentials: true,
+});
+
+export const leaderboardSocket = io.connect('https://art-quest-server.herokuapp.com/leaderboard-namespace', {
+  withCredentials: true,
+});
