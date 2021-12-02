c08dba5740014ca4c397f0a3ab8189074b413d08
khyatith <thakur.khyati70@gmail.com>
2021-12-01 21:16:33 -0800

    Add indicator for new code- temp change

--- src/components/LaunchScreen.jsx
+++ src/components/LaunchScreen.jsx
@@ -126,7 +126,7 @@ function LaunchScreen() {
           <AppBar className={classes.appbar} position="static">
             <Toolbar>
               <Typography variant="h6" className={classes.title}>
-                ART QUEST
+                ART QUEST NEW CODE IS RUNNING
               </Typography>
             </Toolbar>
           </AppBar>
