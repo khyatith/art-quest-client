0cb2a1c81eac57e01b4f10ff16e164d5eb86ccd9
Dezx20 <2009awektoppo@gmail.com>
2022-05-14 22:27:24 +0530

    ...

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -2,11 +2,6 @@ import React from 'react';
 // import LandingPage from './LandingPage';
 // import LocationPhase from './selling/LocationPhase';
 import LandingPage from './buying/LandingPage'; // using this one
-<<<<<<< HEAD
-// import LandingPage from './LandingPage';
-// import LocationPhase from './selling/LocationPhase';
-=======
->>>>>>> 3908380... done dutchAuctionPage + LiveLocationUpdateInAirport
 
 function Game() {
   return (
